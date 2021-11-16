fd1e804b1a3a21ab82a2d4908e2c95dd0b41fb5b
kavitarajput <rajputkavita685@gmail.com>
2021-11-16 11:31:14 +0530

    added;

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+
+const routes: Routes = [
+{path:'employee-dashboard' , component: EmployeeDashboardComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
